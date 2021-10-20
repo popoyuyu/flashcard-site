fa29d752593981ea81b955040ddff67a53e3d0dd
Jeff Lai <pjefflai@gmail.com>
2021-10-19 23:36:18 -0700

    fixed all bugs

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,97 @@
 $(document).ready(function () {
-  $(".all").click(function () {
+  $(".first").click(function () {
     $("#card1").show();
     $("#card1-1").toggle();
   });
+
+  $(".second").click(function () {
+    $("#card2").show();
+    $("#card2-2").toggle();
+  });
+
+  $(".third").click(function () {
+    $("#card3").show();
+    $("#card3-3").toggle();
+  });
+
+  $(".fourth").click(function () {
+    $("#card4").show();
+    $("#card4-4").toggle();
+  });
+
+  $(".fifth").click(function () {
+    $("#card5").show();
+    $("#card5-5").toggle();
+  });
+
+  $(".sixth").click(function () {
+    $("#card6").show();
+    $("#card6-6").toggle();
+  });
+
+  $(".seventh").click(function () {
+    $("#card7").show();
+    $("#card7-7").toggle();
+  });
+
+  $(".eighth").click(function () {
+    $("#card8").show();
+    $("#card8-8").toggle();
+  });
+
+  $(".ninth").click(function () {
+    $("#card9").show();
+    $("#card9-9").toggle();
+  });
+
+  $(".tenth").click(function () {
+    $("#card10").show();
+    $("#card10-10").toggle();
+  });
+
+  $(".eleventh").click(function () {
+    $("#card11").show();
+    $("#card11-11").toggle();
+  });
+
+  $(".twelfth").click(function () {
+    $("#card12").show();
+    $("#card12-12").toggle();
+  });
+
+  $(".thirteenth").click(function () {
+    $("#card13").show();
+    $("#card13-13").toggle();
+  });
+
+  $(".fourteenth").click(function () {
+    $("#card14").show();
+    $("#card14-14").toggle();
+  });
+
+  $(".fifteenth").click(function () {
+    $("#card15").show();
+    $("#card15-15").toggle();
+  });
+
+  $(".sixteenth").click(function () {
+    $("#card16").show();
+    $("#card16-16").toggle();
+  });
+
+  $(".seventeenth").click(function () {
+    $("#card17").show();
+    $("#card17-17").toggle();
+  });
+
+  $(".eighteenth").click(function () {
+    $("#card18").show();
+    $("#card18-18").toggle();
+  });
+
+  $(".nineteenth").click(function () {
+    $("#card19").show();
+    $("#card19-19").toggle();
+  });
+
 });
